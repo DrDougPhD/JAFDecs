77d8d233546d5a207614cb60682e39f07058bf18
Doug McGeehan <doug@kmnr.org>
2023-07-01 11:24:06 -0600

    ready for a new release, almost

--- samples/function-value-initializer.py
+++ samples/function-value-initializer.py
@@ -11,6 +11,29 @@ from jafdecs import initialize, utilities
 random.seed(0)
 
 
+def naive_function(path: pathlib.Path):
+    # This code is somewhat complicated in that it initializes an asset before using it, unless the asset already exists.
+    if not path.exists():
+        print(f'File at {path} does not exist, so we will generate it before calling the actual function that needs it')
+        generated_value = {}
+        n = 18
+        for i in range(10):
+            key = randomstring(n=n)
+            value = randomstring(n=n)
+            generated_value[key] = value
+
+        print('Sleeping to simulate a hard-to-compute function.')
+        time.sleep(2)
+        with path.open('w') as file:
+            json.dump(generated_value, file)
+
+    print(f'File at {path} now exists, so we can get its data.')
+    with path.open() as file:
+        value = json.load(file)
+    
+    pprint(value)
+
+
 def priming_function(path: pathlib.Path):
     print(f'File at {path} does not exist, so we will generate it before calling the actual function that needs it')
     generated_value = {}
